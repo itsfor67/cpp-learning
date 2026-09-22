af81a465e178284414aa01fd082ef5a12ecf17e3
itsfor67 <gamingviper333@gmail.com>
2026-09-22 17:23:58 +0530

    counting

---
+++ counting.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void counting(int a){
+
+    
+
+    for (int i = 1; i <= a; i++){
+
+        cout << i << endl;
+
+    }
+    
+}
+
+
+int main (){
+
+    int a;
+
+    cin >> a;
+
+    counting(a);
+}
